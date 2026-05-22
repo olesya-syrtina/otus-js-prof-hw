f25db889e5431ba7b956f6969312d2b8bc557389
Олеся Сыртина <olesademcenko87091@gmail.com>
2026-05-22 13:31:56 +0300

    ДЗ:Клиентский роутинг и мини‑SPA  - выполнено

--- hw-03/historyAPI.js
+++ hw-03/historyAPI.js
@@ -1,46 +1,73 @@
-window.addEventListener('load', function() {
-    console.log('Страница загрузилась');
-})
-
 const root = document.querySelector('#root');
 
-const routes = {
-    '/': 
-    `
-    <h2>Home</h2>
-    <p>Welcome to the Home page!</p>`,
+const state = {
+   username: localStorage.getItem('username') || 'Гость'
+};
 
-    '/about': 
-    `
-    <h2>About</h2>
-    <p>This is About page!</p>`,
+const routes = [
+    {
+        path: '/',
+        render: () => {
+            root.innerHTML =  `<h2>Домашняя страница</h2>
+            <p>Welcome, ${state.username}!</p>`;
+        }
+    },
+    {
+        path: '/about',
+        render: () => {
+            root.innerHTML = `
+        <h2>О нас</h2>
+        <p>This is About page!</p>`;
+        }
+    },
+    {
+        path: '/settings',
+        render: () => {
+            root.innerHTML =  `
+        <h2>Настройки</h2>
+        <form id="form">
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Ведите свое имя"
+                    value="${state.username}"
+                />
 
-    '/contact': 
-    `
-    <h2>Contact</h2>
-    <p>This is contact page!</p>`,
-}
+                <button type="submit">Сохранить</button>
+            </form>    `;
+
+            const form = document.querySelector('#form');
+
+            form.onsubmit = (event) => {
+                event.preventDefault();
+
+                const input = document.querySelector('#username');
+
+                state.username = input.value;
+                localStorage.setItem('username', input.value);
+
+                input.value = '';
+            };
+        }
+    }
+]
 
 const render = () => {
-    const rout = location.pathname;
-    console.log(rout);
     
-    const page = routes[rout];
+    const route = routes.find((route) => {
+     return route.path === location.pathname;
+    });
 
-    if (page) {
-        root.innerHTML = page;
+
+    if (route) {
+        route.render();
+
     } else {
         root.innerHTML = `
         <h2>404 Not Found</h2>
-
         `
     }
 }
-
-window.history.go(-2);
-
-window.history.forward();
-
 
 window.addEventListener( 'load', () => {
     render();
@@ -55,4 +82,10 @@ document.body.addEventListener('click', (event) => {
     if (!event.target.matches('a')) return;
     
     event.preventDefault();
+
+    const url= event.target.getAttribute('href');
+
+    history.pushState(null, '', url);
+
+    render();
 })
